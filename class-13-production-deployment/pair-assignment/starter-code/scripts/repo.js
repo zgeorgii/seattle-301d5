dc273b1082ef4007b4303b829568ac8bdfd12ffd
zach georgii <zgeorgii@gmail.com>
2016-02-17 12:26:38 -0800

    added Procfile

--- class-13-production-deployment/pair-assignment/starter-code/scripts/repo.js
+++ class-13-production-deployment/pair-assignment/starter-code/scripts/repo.js
@@ -5,15 +5,9 @@
 
   // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/brookr/repos' +
-            '?per_page=100' +
-            '&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
-      success: function(data, message, xhr) {
+    $.get('/users/brookr/repos' + '?per_page=100' + '&sort=updated')
+      .done(function(data, message, xhr) {
         repos.all = data;
-      }
     }).done(callback);
   };
 
